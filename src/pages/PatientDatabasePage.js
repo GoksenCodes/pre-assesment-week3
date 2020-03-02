72e241b33d964c226fb91279a1f2c0ff9efd1fa4
Goksen <goksengorgulu@gmail.com>
2020-03-03 00:40:06 +0100

    add filter by id function for patientdatabase

--- src/pages/PatientDatabasePage.js
+++ src/pages/PatientDatabasePage.js
@@ -1,27 +1,57 @@
 import React, { useState, useEffect } from "react";
 import Patient from "../components/Patient";
 import axios from "axios";
+import Doctor from "../components/Doctor";
 
 export default function PatientDatabasePage() {
   const [patients, setPatients] = useState([]);
+  const [doctors, setDoctors] = useState([]);
 
-  const search = async () => {
+  function handleChange(event) {
+    const search = async () => {
+      const response = await axios.get(
+        "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients"
+      );
+      const patientList = response.data;
+
+      const filteredPatients = patientList.filter(patient => {
+        if (patient.doctorId == doctorId) {
+          return patient;
+        }
+      });
+      console.log(filteredPatients); //how to display it
+    };
+    const doctorId = event.target.value;
+    search();
+    // console.log(response.data);
+  }
+
+  // useEffect(() => {
+  //   search();
+  // }, []);
+
+  const getDoctors = async () => {
     const response = await axios.get(
-      "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/patients"
+      "https://my-json-server.typicode.com/Codaisseur/patient-doctor-data/doctors"
     );
 
-    setPatients(response.data);
-
+    setDoctors(response.data);
     console.log(response.data);
   };
 
   useEffect(() => {
-    search();
+    getDoctors();
   }, []);
 
   return (
     <div>
-      <h4>Patient Database</h4>;
+      <h4>Patient Database</h4>
+      <select onChange={handleChange}>
+        <option>Choose a doctor</option>
+        {doctors.map(doctor => (
+          <option value={doctor.id}>{doctor.doctor}</option>
+        ))}
+      </select>
       {patients.map(patient => {
         return (
           <Patient
